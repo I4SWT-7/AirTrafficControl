005027ddc359afbecbe3c17f9519b53d25228538
MHJespersen <mathiasholsko@hotmail.com>
2019-10-23 14:07:42 +0200

    Adding parser folder

---
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Parser/FlyParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+
+namespace TransponderReceiverApplication
+{
+    class FlyParser : IParser
+    {
+        List<Fly> FlyList = new List<Fly>();
+        List<String> SavedData = new List<string>();
+        private ITransponderReceiver receiver;
+
+        public FlyParser(ITransponderReceiver receiver)
+        {
+            // This will store the real or the fake transponder data receiver
+            this.receiver = receiver;
+
+            // Attach to the event of the real or the fake TDR
+            this.receiver.TransponderDataReady += RecieveData;
+        }
+
+        public void Parsedata(string data)
+        {
+            // create new Fly object, insert data from parameter into fly object and save to fly list   
+            Console.WriteLine($"FlyParserData {data}");
+        }
+
+
+        private void RecieveData(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                Parsedata(data);
+            }
+        }
+    }
+}
---
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Parser/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverApplication
+{
+    interface IParser
+    {
+
+        void Parsedata(string data);
+        //unkown event sender / handler has to be added to send data to transformer
+    }
+
+}
