380e43f45295dbb44722ee54232aa44bc3ddbf01
Mr. Jones <jonastaekke@gmail.com>
2019-10-30 18:42:38 +0100

    Oprettet filer til filter

---
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Filter/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverApplication.Filter
+{
+    class Filter : IFilter
+    {
+        event EventHandler<RawFilterDataEventArgs> IFilter.FilterDataReady
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        void IFilter.Filterdata(List<Fly> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Filter/RawFilterDataEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverApplication.Filter
+{
+    public class RawFilterDataEventArgs : EventArgs
+    {
+        public List<Fly> FlyList { get; set; }
+
+        public RawFilterDataEventArgs(List<Fly> flyList)
+        {
+            FlyList = flyList;
+        }
+    }
+}
