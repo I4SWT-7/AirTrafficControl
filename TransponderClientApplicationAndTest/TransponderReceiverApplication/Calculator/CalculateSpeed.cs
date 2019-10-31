adcb4cf4bc5d7225570c8eb1b356c9585d720862
MikkelMahler <mmmikkel@hotmail.com>
2019-10-31 20:10:28 +0100

    Adding Calculation tests

--- TransponderClientApplicationAndTest/TransponderReceiverApplication/Calculator/CalculateSpeed.cs
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Calculator/CalculateSpeed.cs
@@ -1,23 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace TransponderReceiverApplication.Calculator
-//{
-//    class CalculateSpeed
-//    {
-//        private int distance;
-//        private int speed;
-//        private int timeStamps;
-//        int CalcSpeed(List<Fly> watchlist)
-//        {
-//            // To calculate the speed of an airplane we need two different timestamps from it
-//            timeStamps = Fly.date1 - Fly.date2;
+namespace TransponderReceiverApplication.Calculator
+{
+    public class CalculateSpeed
+    {
+        private int distance;
+        private int speed;
+        private int timeStamps;
+        public int CalcSpeed(List<Fly> watchlist)
+        {
+            // To calculate the speed of an airplane we need two different timestamps from it
+            timeStamps = Fly.date1 - Fly.date2;
 
-//            speed = distance / timeStamps;
-//            return speed;
-//        }
-//    }
-//}
+            speed = distance / timeStamps;
+            return speed;
+        }
+    }
+}
