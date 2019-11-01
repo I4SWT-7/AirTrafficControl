5e3f71e6d9530f3b86b3f261ce6440ca7550052c
Andreas Ventzel <au280318@uni.au.dk>
2019-11-01 23:21:21 +0100

    Added TestConsoleDisplay + Modified ConsoleDisplay

---
+++ TransponderClientApplicationAndTest/TestFlyParser/TestConsoleDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TransponderReceiverApplication;
+namespace TestFlyParser
+{
+    [TestFixture]
+    class TestConsoleDisplay
+    {
+        
+        Fly testfly = new Fly("TAG321", 9000, 9000, 1000);
+        private double testspeed = 9000;
+        private double testcourse = 90;
+
+        private ConsoleDisplay _uut;
+
+        [Test]
+        public void ConsoleDisplay_writes_correct_plane()
+        {
+            _uut = new ConsoleDisplay();
+            string teststring =  "Displaying plane with tag:TAG321, x position 9000meters, y position 9000 meters, altitude: 1000, velocity: 9000 course: 90 degrees ";
+            _uut.PrintPlane(testfly, testspeed, testcourse);
+            Assert.That(_uut.PrintPlane(testfly, testspeed, testcourse), Is.EqualTo (teststring));
+        }
+    }
+}
--- TransponderClientApplicationAndTest/TransponderReceiverApplication/ConsoleDisplay.cs
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/ConsoleDisplay.cs
@@ -8,14 +8,17 @@ namespace TransponderReceiverApplication
 {
    public class ConsoleDisplay
     {
-        public void PrintPlane(Fly fly, double speed, double course)
+        public string PrintPlane(Fly fly, double speed, double course)
         {
-               //Tag(text string, 6 characters)
-               // • Current position(x-y, both in meters)
-               // • Current altitude(meters)
-               // • Current horizontal velocity(m / s)
-               // • Current compass course(0 - 359 degrees, clockwise, 0 degrees is north)
-               Console.WriteLine($"Displaying plane with tag:{fly.Tag}, x position {fly.xcor}meters, y position {fly.ycor} meters, altitude: {fly.zcor}, velocity: {speed} course: {course} degrees ");
+            //Tag(text string, 6 characters)
+            // • Current position(x-y, both in meters)
+            // • Current altitude(meters)
+            // • Current horizontal velocity(m / s)
+            // • Current compass course(0 - 359 degrees, clockwise, 0 degrees is north)
+            string displaystring =
+                $"Displaying plane with tag:{fly.Tag}, x position {fly.xcor}meters, y position {fly.ycor} meters, altitude: {fly.zcor}, velocity: {speed} course: {course} degrees ";
+            Console.WriteLine(displaystring);
+            return displaystring;
         }
     }
 }
