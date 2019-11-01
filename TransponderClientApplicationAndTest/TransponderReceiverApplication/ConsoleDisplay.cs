5bf6c18c0321196142f6ba1a7135a5745ac7009a
MHJespersen <mathiasholsko@hotmail.com>
2019-11-01 18:48:52 +0100

    Adding consoldisplay

---
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/ConsoleDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiverApplication
+{
+   public class ConsoleDisplay
+    {
+        public void PrintPlane(Fly fly, double speed, double course)
+        {
+               //Tag(text string, 6 characters)
+               // • Current position(x-y, both in meters)
+               // • Current altitude(meters)
+               // • Current horizontal velocity(m / s)
+               // • Current compass course(0 - 359 degrees, clockwise, 0 degrees is north)
+               Console.WriteLine($"Displaying plane with tag:{fly.Tag}, x position {fly.xcor}meters, y position {fly.ycor} meters, altitude: {fly.zcor}, velocity: {speed} course: {course} degrees ");
+        }
+    }
+}
