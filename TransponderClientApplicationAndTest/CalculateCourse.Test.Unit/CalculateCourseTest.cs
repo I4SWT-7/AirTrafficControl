04020b78dadf0e5b323068b6c7f8370aebf20c00
MikkelMahler <mmmikkel@hotmail.com>
2019-11-01 19:32:44 +0100

    Finished all Calculator Tests

--- TransponderClientApplicationAndTest/CalculateCourse.Test.Unit/CalculateCourseTest.cs
+++ TransponderClientApplicationAndTest/CalculateCourse.Test.Unit/CalculateCourseTest.cs
@@ -13,8 +13,16 @@ namespace CalculateCourse.Test.Unit
     {
         Fly Fly1 = new Fly("TAG123", 1, 1, 500);
         Fly Fly2 = new Fly("TAG123", 100, 100, 1000);
+        Fly Fly3 = new Fly("TAG123", 1, 1, 500);
+        Fly Fly4 = new Fly("TAG123", 1, 100, 1000);
+        Fly Fly5 = new Fly("TAG123", 1, 1, 500);
+        Fly Fly6 = new Fly("TAG123", 1, -100, 1000);
+        Fly Fly7 = new Fly("TAG123", 1, 1, 500);
+        Fly Fly8 = new Fly("TAG123", 100, 1, 1000);
+        Fly Fly9 = new Fly("TAG123", 1, 1, 500);
+        Fly Fly10 = new Fly("TAG123", -100, 1, 1000);
         [Test]
-        public void CalcCourse_45degrees()
+        public void CalcCourse_NorthWest()
         {
             //Arrange
             var uut = new TransponderReceiverApplication.CalculateCourse();
@@ -22,13 +30,42 @@ namespace CalculateCourse.Test.Unit
             // Act & Assert
             Assert.That(uut.CalcCourse(Fly1, Fly2), Is.EqualTo(45));
         }
-        public void CalcCourse_180degrees()
+        [Test]
+        public void CalcCourse_North()
         {
             //Arrange
             var uut = new TransponderReceiverApplication.CalculateCourse();
 
             // Act & Assert
-            Assert.That(uut.CalcCourse(Fly1, Fly2), Is.EqualTo(45));
+            Assert.That(uut.CalcCourse(Fly3, Fly4), Is.EqualTo(0));
+        }
+        [Test]
+        public void CalcCourse_South()
+        {
+            //Arrange
+            var uut = new TransponderReceiverApplication.CalculateCourse();
+
+            // Act & Assert
+            Assert.That(uut.CalcCourse(Fly5, Fly6), Is.EqualTo(180));
+        }
+        [Test]
+        public void CalcCourse_West()
+        {
+            //Arrange
+            var uut = new TransponderReceiverApplication.CalculateCourse();
+
+            // Act & Assert
+            Assert.That(uut.CalcCourse(Fly7, Fly8), Is.EqualTo(90));
+        }
+        [Test]
+        public void CalcCourse_East()
+        {
+            //Arrange
+            var uut = new TransponderReceiverApplication.CalculateCourse();
+
+            // Act & Assert
+            Assert.That(uut.CalcCourse(Fly9, Fly10), Is.EqualTo(270));
         }
     }
 }
+
--- TransponderClientApplicationAndTest/TransponderReceiverApplication/Calculator/SeperationCondition.cs
+++ TransponderClientApplicationAndTest/TransponderReceiverApplication/Calculator/SeperationCondition.cs
@@ -10,7 +10,7 @@ namespace TransponderReceiverApplication
     {
         private CalculateDistance calcdistance = new CalculateDistance();
         private WriteLog logger = new WriteLog();
-        public void SepCond(Fly prevfly, Fly Newfly)
+        public bool SepCond(Fly prevfly, Fly Newfly)
         {
             int biggestzvalue;
             int smallestzvalue;
@@ -30,8 +30,11 @@ namespace TransponderReceiverApplication
                 if (prevfly.Tag != Newfly.Tag && calcdistance.CalcDistance(Newfly, prevfly) < 5000)
                 {
                     logger.WriteLogWarning(prevfly, Newfly);
+                    return true;
                 }
             }
+
+            return false;
         }
     }
 }
